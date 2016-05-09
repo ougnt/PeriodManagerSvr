8027972cba9c1d7ef203f0d0967dd16fab96dfd9
Wacharin Tangseree <ougntnarak@gmail.com>
2016-05-09 13:50:45 +0700

    Update ads experiment

--- conf/db_updates.sql
+++ conf/db_updates.sql
@@ -339,3 +339,85 @@ VALUES
 	null,
 	1
 );
+
+-- -------------------------------------------
+-- version 7.0
+-- -------------------------------------------
+
+UPDATE db_info
+SET db_version = 7,
+  `rec_modified_by`= `rec_created_by`,
+  `rec_modified_when` = CURRENT_TIMESTAMP();
+
+UPDATE		experiment_ads_run
+SET				rec_status = 0
+WHERE		rec_status = 1;
+
+INSERT INTO experiment_ads_run
+(
+	experiment_id,
+	displayed_language,
+	a_ads_url,
+	a_ads_text,
+	a_ads_show,
+	a_ads_click,
+	b_ads_url,
+	b_ads_text,
+	b_ads_show,
+	b_ads_click,
+	c_ads_url,
+	c_ads_text,
+	c_ads_show,
+	c_ads_click,
+	d_ads_url,
+	d_ads_text,
+	d_ads_show,
+	d_ads_click,
+	e_ads_url,
+	e_ads_text,
+	e_ads_show,
+	e_ads_click,
+	f_ads_url,
+	f_ads_text,
+	f_ads_show,
+	f_ads_click,
+	rec_created_by,
+	rec_created_when,
+	rec_modified_by,
+	rec_modified_when,
+	rec_status
+)
+VALUES
+(
+	(SELECT experiment_id FROM experiment LIMIT 1),
+	'th',
+	'http://ho.lazada.co.th/SHGDGu?sku=IB365HBAA1TDZ4ANTH&redirect=http%3A%2F%2Fho.lazada.co.th%2FSHGDGs%3Furl%3Dhttp%253A%252F%252Fwww.lazada.co.th%252Fibabi-lh-ovulation-test-strip-3-3050752.html%253Foffer_id%253D%257Boffer_id%257D%2526affiliate_id%253D%257Baffiliate_id%257D%2526offer_name%253D%257Boffer_name%257D_%257Boffer_file_id%257D%2526affiliate_name%253D%257Baffiliate_name%257D%2526transaction_id%253D%257Btransaction_id%257D',
+	N'ส่งฟรี อุปกรณ์ตรวจไข่ตก',
+	0,
+	0,
+	'http://ho.lazada.co.th/SHGDGu?sku=PH250HBAA2CK7SANTH&redirect=http%3A%2F%2Fho.lazada.co.th%2FSHGDGs%3Furl%3Dhttp%253A%252F%252Fwww.lazada.co.th%252Fphecare-3945304.html%253Foffer_id%253D%257Boffer_id%257D%2526affiliate_id%253D%257Baffiliate_id%257D%2526offer_name%253D%257Boffer_name%257D_%257Boffer_file_id%257D%2526affiliate_name%253D%257Baffiliate_name%257D%2526transaction_id%253D%257Btransaction_id%257D&aff_sub=pregnancy_test_cheap',
+	N'ส่งฟรี ชุดทดสอบการตั้งครรภ์แบบจุ่ม',
+	0,
+	0,
+	'http://ho.lazada.co.th/SHGDGu?sku=KL511HBAPQRTANTH&redirect=http%3A%2F%2Fho.lazada.co.th%2FSHGDGs%3Furl%3Dhttp%253A%252F%252Fwww.lazada.co.th%252Fklick-as-3-734537.html%253Foffer_id%253D%257Boffer_id%257D%2526affiliate_id%253D%257Baffiliate_id%257D%2526offer_name%253D%257Boffer_name%257D_%257Boffer_file_id%257D%2526affiliate_name%253D%257Baffiliate_name%257D%2526transaction_id%253D%257Btransaction_id%257D&aff_sub=pregnancy_test_expensive',
+	N'ส่งฟรี ชุดทดสอบการตั้งครรภ์แบบปากกา',
+	0,
+	0,
+	'http://ho.lazada.co.th/SHGDGu?sku=OE857HBAA225NNANTH&redirect=http%3A%2F%2Fho.lazada.co.th%2FSHGDGs%3Furl%3Dhttp%253A%252F%252Fwww.lazada.co.th%252Fshecan-1-3459875.html%253Foffer_id%253D%257Boffer_id%257D%2526affiliate_id%253D%257Baffiliate_id%257D%2526offer_name%253D%257Boffer_name%257D_%257Boffer_file_id%257D%2526affiliate_name%253D%257Baffiliate_name%257D%2526transaction_id%253D%257Btransaction_id%257D&aff_sub=shecan',
+	N'ส่งฟรี อุปกรณ์ยืนปัสสาวะสำหรับสตรี',
+	0,
+	0,
+	'http://ho.lazada.co.th/SHGDGu?sku=SA564HBAA2OH4WANTH&redirect=http%3A%2F%2Fho.lazada.co.th%2FSHGDGs%3Furl%3Dhttp%253A%252F%252Fwww.lazada.co.th%252Fsanita-10-3-4501184.html%253Foffer_id%253D%257Boffer_id%257D%2526affiliate_id%253D%257Baffiliate_id%257D%2526offer_name%253D%257Boffer_name%257D_%257Boffer_file_id%257D%2526affiliate_name%253D%257Baffiliate_name%257D%2526transaction_id%253D%257Btransaction_id%257D&aff_sub=sanita',
+	N'ส่งฟรี ผ้าอนามัยแบบห่วง สำหรับสตรีคลอดบุตร ',
+	0,
+	0,
+	'http://ho.lazada.co.th/SHGDGu?sku=OE857HBBP2IXANTH&redirect=http%3A%2F%2Fho.lazada.co.th%2FSHGDGs%3Furl%3Dhttp%253A%252F%252Fwww.lazada.co.th%252Ftravelmate-25-1916169.html%253Foffer_id%253D%257Boffer_id%257D%2526affiliate_id%253D%257Baffiliate_id%257D%2526offer_name%253D%257Boffer_name%257D_%257Boffer_file_id%257D%2526affiliate_name%253D%257Baffiliate_name%257D%2526transaction_id%253D%257Btransaction_id%257D&aff_sub=travelmate',
+	N'ส่งฟรี แผ่นรองนั่งโถส้วม กันเชื้อโรค',
+	0,
+	0,
+	(SELECT user_id FROM users LIMIT 1),
+	'2016-04-22T20:29:47.578+07:00',
+	null,
+	null,
+	1
+);
